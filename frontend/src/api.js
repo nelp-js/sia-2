54cc74eaae2801f04c3c139a7f611bac99aaf513
nelp <aineltbacus@gmail.com>
2026-01-20 03:29:25 +0800

    debugging

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
+  baseURL: "https://sia-2.onrender.com",
 });
 
 api.interceptors.request.use(
